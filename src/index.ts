f554e2e76afc72955ed802b522227295469d6993
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-03-28 11:49:58 -0600

    Export QueryPart type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Database } from "./structures/database";
 import { Table } from "./structures/table";
-import { Query } from "./syntax/builder";
+import { Query, QueryPart } from "./syntax/builder";
 
 import { keywords, reservedKeywords, Keyword, ReservedKeyword } from "./syntax/keywords";
 
-export { Database, Table, Query, keywords, reservedKeywords, Keyword, ReservedKeyword };
+export { Database, Table, Query, keywords, reservedKeywords, Keyword, ReservedKeyword, QueryPart };
